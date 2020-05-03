33c712b92254d6e38835ee8d3e9fe296f56ec0df
reemehab <60987184+reemehab@users.noreply.github.com>
2020-05-03 19:36:57 +0200

    recook

---
+++ js/jquery.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+$('#select').hover( function(){
+
+$('#hide').toggle(function(){});
+;
+
+});
+
+$('#selects').hover( function(){
+
+    $('#hides').toggle(function(){});
+    ;
+    
+    });
+
+    $('#selectz').hover( function(){
+
+        $('#hidez').toggle(function(){});
+        ;
+        
+        });
+
+
+
+});
